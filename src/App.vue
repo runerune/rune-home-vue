32c8d18ba2914b28bce075c59763b132f223c1c1
runerune <guildbot@bezkr.es>
2020-08-10 14:51:45 +0200

    add pano component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
+<template>
+	<div id="app">
+		<Pano></Pano>
+		<Index></Index>
+	</div>
+</template>
+
+<script>
+import Index from './components/Index.vue'
+import Pano from './components/Pano.vue'
+
 export default {
 	name: 'App',
 	components: {
-		HelloWorld
+		Index,
+		Pano
 	}
 }
 </script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ src/components/Pano.vue
@@ -0,0 +1,69 @@
+<template>
+	<aside id="pano-container" role="presentation"></aside>
+</template>
+
+<script>
+
+import 'pannellum/build/pannellum.js';
+import 'pannellum/build/pannellum.css';
+
+export default {
+	name: 'Index',
+
+	data: () => {
+		return {
+			pano: undefined
+		}
+	},
+
+	methods: {
+		getHfov: (width) => {
+			if(width < 1000) return 60;
+			if(width < 1400) return 80;
+
+			return 100;
+		},
+
+		getUrl: (width) => {
+			if(width <= 640) return require('../assets/pano-background-small.jpg');
+			return require('../assets/pano-background.jpg');
+		}
+	},
+
+	mounted: function() {
+		// eslint-disable-next-line no-undef
+		this.pano = pannellum.viewer('pano-container', {
+			type: 'equirectangular',
+			panorama: this.getUrl(window.innerWidth),
+			preview: require('../assets/pano-preview.jpg'),
+			keyboardZoom: false,
+			disableKeyboardCtrl: true,
+			mouseZoom: false,
+			friction: 0.01,
+			pitch: -25,
+			yaw: 10,
+			maxPitch: 38,
+			autoRotate: -2,
+			autoLoad: true,
+			autoRotateInactivityDelay: 3*1000,
+			autoRotateStopDelay: 1,
+			showControls: false,
+			draggable: false,
+			hfov: this.getHfov(window.innerWidth),
+		});
+	},
+
+
+
+}
+</script>
+
+<style scoped>
+	#pano-container {
+		width: 100%;
+		height: 100%;
+		position: fixed;
+		top: 0;
+		left: 0;
+	}
+</style>
