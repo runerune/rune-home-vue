14a23cd21d0ea69285c70f0b79a8689bbea3907b
runerune <guildbot@bezkr.es>
2020-08-10 17:24:29 +0200

    add footer and video components

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+	<footer class="footer">
+		<p class="footer-inner">&copy; 2020 HORIZON PHOTOGRAPHY</p>
+	</footer>
+</template>
+
+<style scoped>
+	.footer {
+		position: relative;
+		z-index: 50;
+		font-size: .9rem;
+		text-align: center;
+	}
+
+	.footer-inner {
+		margin: 2rem auto 4rem;
+		width: 100%;
+		display: block;
+		padding: .4rem 0;
+		box-sizing: border-box;
+		color: #eee;
+		text-shadow: 0 0 .3rem #000;
+	}
+</style>
---
+++ src/components/Video.vue
@@ -0,0 +1,69 @@
+<template>
+	<section class="video">
+		<h2 class="hide">Featured video</h2>
+		
+		<iframe src="https://www.youtube.com/embed/CboI5d5hxdg?version=3&modestbranding=1&showinfo=0&autohide=1&controls=1&vq=hd720"></iframe>
+	</section>
+</template>
+
+<style scoped>
+	.video {
+		position: relative;
+		z-index: 50;
+		width: 100%;
+		backdrop-filter: contrast(70%) blur(1rem);
+	}
+
+	@-moz-document url-prefix() {
+		.video {
+			background: url('^assets/blur-fallback.jpg') top fixed no-repeat;
+			background-size: cover;
+			backdrop-filter: none;
+		}
+	}
+
+	@media only screen and (max-width: 720px) {
+		.video {
+			background: url('^assets/blur-fallback.jpg') top fixed no-repeat;
+			background-size: cover;
+			backdrop-filter: none;
+		}
+	}
+
+	.video iframe {
+		width: 1280px;
+		height: 720px;
+		margin: 0 auto;
+		padding: 4rem 0;
+		display: block;
+	}
+
+	@media only screen and (max-width: 1420px) {
+		.video iframe {
+			width: 960px;
+			height: 540px;
+		}
+	}
+
+	@media only screen and (max-width: 1120px) {
+		.video iframe {
+			width: 720px;
+			height: 360px;
+		}
+	}
+
+	@media only screen and (max-width: 860px) {
+		.video iframe {
+			width: 540px;
+			height: 270px;
+		}
+	} 
+
+	@media only screen and (max-width: 690px) {
+		.video iframe {
+			width: 100%;
+			max-width: 405px;
+			height: 202px;
+		}
+	}
+</style>
