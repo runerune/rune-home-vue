7f60087572b5349bbd5cb8e4899ec408406af893
runerune <guildbot@bezkr.es>
2020-08-10 15:56:45 +0200

    add fragment plugin for templates

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Fragment from 'vue-fragment';
+
+Vue.use(Fragment.Plugin);
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
 
 new Vue({
 	render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
